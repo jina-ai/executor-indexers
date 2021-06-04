e9b842d1e6b55fd59dc443680440ec2ecb82de50
Cristian <cristian.mitroi@jina.ai>
2021-06-04 15:16:04 +0200

    ci: use venv

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,9 +1,16 @@
+sudo apt-get update && sudo apt-get install python3-venv
+
 test_dirs=`find . -name "tests*" -type d`
+echo $test_dirs
 
 EXIT_CODE=0
 
-for test_dir in ${tests_dirs[@]}; do
-  executor_dir="$(dirname "test_dir")"
+root_dir=$(pwd)
+
+for test_dir in ${test_dirs[@]}; do
+  echo 'checking' $test_dir
+  executor_dir="$(dirname "$test_dir")"
+  echo 'changing to dir' $executor_dir
   cd $executor_dir
   python -m venv .venv
   pip install pytest pytest-mock
@@ -15,6 +22,7 @@ for test_dir in ${tests_dirs[@]}; do
     EXIT_CODE=$local_exit_code
     echo this one failed. local_exit_code = $local_exit_code, exit = $EXIT_CODE
   fi
+  cd $root_dir
   done
 
 echo final exit code = $EXIT_CODE
