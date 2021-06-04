58ed8ab28788e73019d07c0b69776d50c6432094
Cristian <cristian.mitroi@jina.ai>
2021-06-04 15:16:04 +0200

    ci: use venv

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -5,8 +5,12 @@ EXIT_CODE=0
 for test_dir in ${tests_dirs[@]}; do
   executor_dir="$(dirname "test_dir")"
   cd $executor_dir
+  python -m venv .venv
+  pip install pytest pytest-mock
+  pip install -r requirements.txt
   pytest -s -v tests/
   local_exit_code=$?
+  deactivate
   if [[ ! $local_exit_code == 0 ]]; then
     EXIT_CODE=$local_exit_code
     echo this one failed. local_exit_code = $local_exit_code, exit = $EXIT_CODE
