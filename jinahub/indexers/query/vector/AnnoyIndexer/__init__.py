12e42094fd4051b61096fd50a573d7f2548a22e5
yongxuanzhang <yongxuanzhang93@gmail.com>
2021-06-09 09:25:31 -0400

    fix: add log info

--- jinahub/indexers/query/vector/AnnoyIndexer/__init__.py
+++ jinahub/indexers/query/vector/AnnoyIndexer/__init__.py
@@ -49,6 +49,7 @@ class AnnoyIndexer(Executor):
         self.logger = JinaLogger(self.metas.name)
         dump_path = dump_path or kwargs.get('runtime_args').get('dump_path')
         if dump_path is not None:
+            self.logger.info('Start building "AnnoyIndexer" from dump data')
             ids, vecs = import_vectors(dump_path, str(self.metas.pea_id))
             self._ids = np.array(list(ids))
             self._vecs = np.array(list(vecs))
