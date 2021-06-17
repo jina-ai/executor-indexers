493023d5b5869222c9adc54749bb415e79e36ad6
Cristian <cristian.mitroi@jina.ai>
2021-06-17 12:24:36 +0200

    fix: delete invalid entries on close

--- jinahub/indexers/dbms/FileDBMSIndexer/tests/test_indexer.py
+++ jinahub/indexers/dbms/FileDBMSIndexer/tests/test_indexer.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 
-from jina import Document, DocumentArray
+from jina import Document, DocumentArray, Flow
 
 from jina_commons.indexers.dump import import_vectors, import_metas
 from .. import FileDBMSIndexer
@@ -11,12 +11,12 @@ d_embedding = np.array([1, 1, 1, 1, 1, 1, 1])
 c_embedding = np.array([2, 2, 2, 2, 2, 2, 2])
 
 
-def get_documents(nr=10, index_start=0, emb_size=7):
+def get_documents(nr=10, index_start=0, emb_size=7, text='hello world'):
     docs = []
     for i in range(index_start, nr + index_start):
         with Document() as d:
             d.id = i
-            d.text = f'hello world {i}'
+            d.text = f'{text} {i}'
             d.embedding = np.random.random(emb_size)
             d.tags['field'] = f'tag data {i}'
         docs.append(d)
@@ -36,7 +36,7 @@ def assert_dump_data(dump_path, docs, shards, pea_id):
         ]
     else:
         docs_expected = docs[(pea_id) * size_shard : (pea_id + 1) * size_shard]
-    print(f'### pea {pea_id} has {len(docs_expected)} docs')
+    # pea {pea_id} has {len(docs_expected)} docs
 
     # TODO these might fail if we implement any ordering of elements on dumping / reloading
     ids_dump = list(ids_dump)
@@ -85,7 +85,9 @@ def test_dbms_file_writer(tmpdir):
     new_docs = get_documents(nr=10)
 
     # assert contents update
-    with FileDBMSIndexer(index_filename='dbms', metas=metas) as indexer:
+    with FileDBMSIndexer(
+        dump_on_exit=True, index_filename='dbms', metas=metas
+    ) as indexer:
         indexer.update(new_docs)
         assert indexer.size == 2 * len(docs)
         dump_path = indexer.default_dump_path
@@ -93,10 +95,76 @@ def test_dbms_file_writer(tmpdir):
     assert_dump_data(dump_path, docs2 + new_docs, 1, 0)
 
     # assert contents update
-    with FileDBMSIndexer(index_filename='dbms', metas=metas) as indexer:
+    with FileDBMSIndexer(
+        dump_on_exit=True, index_filename='dbms', metas=metas
+    ) as indexer:
         indexer.delete(docs)
         assert indexer.size == len(docs)
         dump_path = indexer.default_dump_path
         print("ALMOST FINISHED DELETING")
 
     assert_dump_data(dump_path, docs2, 1, 0)
+
+
+def test_update_not_growing(tmpdir):
+    docs = get_documents(nr=10)
+    update_docs = get_documents(nr=10, text='hello there')
+
+    metas = {'workspace': str(tmpdir), 'name': 'dbms', 'pea_id': 0, 'replica_id': 0}
+
+    # indexing
+    with FileDBMSIndexer(index_filename='dbms', metas=metas) as indexer:
+        indexer.add(docs)
+
+    # getting size
+    with FileDBMSIndexer(index_filename='dbms', metas=metas) as indexer:
+        size = indexer.physical_size()
+        items = indexer.size
+
+    # updating
+    with FileDBMSIndexer(index_filename='dbms', metas=metas) as indexer:
+        indexer.update(update_docs)
+
+    # asserting...
+    with FileDBMSIndexer(index_filename='dbms', metas=metas) as indexer:
+        assert indexer.physical_size() == size
+        assert indexer.size == items
+
+
+def test_update_not_growing_flow(tmpdir):
+    metas = {'workspace': str(tmpdir), 'name': 'dbms'}
+    runtime_args = {'pea_id': 0, 'replica_id': None}
+
+    def _get_flow() -> Flow:
+        return Flow().add(
+            uses={
+                'jtype': 'FileDBMSIndexer',
+                'with': {'index_filename': 'dbms'},
+                'metas': metas,
+            }
+        )
+
+    docs = get_documents(nr=10)
+    update_docs = get_documents(nr=10, text='hello there')
+
+    # indexing
+    with _get_flow() as f:
+        f.index(inputs=docs)
+
+    # getting size
+    with FileDBMSIndexer(
+        index_filename='dbms', metas=metas, runtime_args=runtime_args
+    ) as indexer:
+        size = indexer.physical_size()
+        items = indexer.size
+
+    # updating
+    with _get_flow() as f:
+        f.post(on='/update', inputs=update_docs)
+
+    # asserting...
+    with FileDBMSIndexer(
+        index_filename='dbms', metas=metas, runtime_args=runtime_args
+    ) as indexer:
+        assert indexer.physical_size() == size
+        assert indexer.size == items
