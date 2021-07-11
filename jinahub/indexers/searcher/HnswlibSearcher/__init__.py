4a29d8479fe18bffc61fba4220d46f7e968032ed
yongxuanzhang <yongxuanzhang93@gmail.com>
2021-07-11 19:20:21 -0400

    feat: topk override

--- jinahub/indexers/searcher/HnswlibSearcher/__init__.py
+++ jinahub/indexers/searcher/HnswlibSearcher/__init__.py
@@ -23,29 +23,25 @@ class HnswlibSearcher(Executor):
 
     def __init__(
         self,
-        top_k: int = 10,
+        default_top_k: int = 10,
         metric: str = 'euclidean',
-        num_trees: int = 10,
         dump_path: Optional[str] = None,
-        index_path: str = './hnswlib.index',
         default_traversal_paths: List[str] = None,
         **kwargs,
     ):
         """
         Initialize an HnswlibSearcher
 
-        :param top_k: get tok k vectors
+        :param default_top_k: get tok k vectors
         :param metric: Metric can be "angular", "euclidean", "manhattan", "hamming", or "dot"
-        :param num_trees: builds a forest of n_trees trees. More trees gives higher precision when querying.
         :param dump_path: the path to load ids and vecs
         :param traverse_path: traverse path on docs, e.g. ['r'], ['c']
         :param args:
         :param kwargs:
         """
         super().__init__(**kwargs)
-        self.top_k = top_k
+        self.default_top_k = default_top_k
         self.metric = metric
-        self.num_trees = num_trees
         self.default_traversal_paths = default_traversal_paths or ['r']
         self.logger = get_logger(self)
         dump_path = dump_path or kwargs.get('runtime_args', {}).get('dump_path', None)
@@ -59,18 +55,16 @@ class HnswlibSearcher(Executor):
             self._indexer.init_index(max_elements=len(self._vecs), ef_construction=400, M=64)
 
             self._doc_id_to_offset = {}
-            self._load_index(self._ids, self._vecs, index_path)
+            self._load_index(self._ids, self._vecs)
         else:
             self.logger.warning(
                 'No data loaded in "HnswlibSearcher". Use .rolling_update() to re-initialize it...'
             )
 
-    def _load_index(self, ids, vecs, index_path):
+    def _load_index(self, ids, vecs):
         for idx, v in enumerate(vecs):
             self._indexer.add_items(v.astype(np.float32),idx)
             self._doc_id_to_offset[ids[idx]] = idx
-        #self._indexer.build(self.num_trees)
-        self._indexer.save_index(index_path)
         self._indexer.set_ef(50)
 
     @requests(on='/search')
@@ -78,13 +72,13 @@ class HnswlibSearcher(Executor):
         if not hasattr(self, '_indexer'):
             self.logger.warning('Querying against an empty index')
             return
-
+        top_k = parameters.get('top_k', self.default_top_k)
         traversal_paths = parameters.get(
             'traversal_paths', self.default_traversal_paths
         )
 
         for doc in docs.traverse_flat(traversal_paths):
-            indices, dists = self._indexer.knn_query(doc.embedding, k=self.top_k)
+            indices, dists = self._indexer.knn_query(doc.embedding, k=top_k)
             for idx, dist in zip(indices[0], dists[0]):
                 match = Document(id=self._ids[idx], embedding=self._vecs[idx])
                 match.scores['distance'] = 1 / (1 + dist)
