f363588c7630eda1363428a493037cb84c8193ab
cristian <cristianmtr@users.noreply.github.com>
2021-07-21 16:52:06 +0200

    ci: add push hub (#75)

--- jinahub/indexers/SimpleIndexer/tests/test_simple_indexer.py
+++ jinahub/indexers/SimpleIndexer/tests/test_simple_indexer.py
@@ -1,7 +1,7 @@
 import numpy as np
 from jina import Flow, Document, DocumentArray
 
-from jinahub.indexers.simple import SimpleIndexer
+from jinahub.indexers.SimpleIndexer import SimpleIndexer
 
 
 def test_simple_indexer_flow(tmpdir):
